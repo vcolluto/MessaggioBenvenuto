8f2812877c4fa851229b67e60b327186b8d79ca9
Enzo Colluto <vcolluto>
2022-10-04 11:58:14 +0200

    modificato messaggio di benvenuto

--- src/com/soprasteria/MessaggioSaluto.java
+++ src/com/soprasteria/MessaggioSaluto.java
@@ -3,7 +3,7 @@ package com.soprasteria;
 public class MessaggioSaluto {
 
 	public static void main(String[] args) {
-		System.out.println("Ciao mondo!");
+		System.out.println("Ciao a tutti!");
 	}
 
 }
